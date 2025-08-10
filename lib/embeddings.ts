aaa15f61d3ca2de6007bef029411044ccfe2bb03
neonhauntco-stack <neonhauntco@gmail.com>
2025-08-10 09:52:28 -0700

    chore: add embeddings stub

---
+++ lib/embeddings.ts
@@ -0,0 +1,7 @@
+export async function embedImage(_base64: string): Promise<number[] | null> {
+  return null // TODO: implementar CLIP/OpenAI
+}
+
+export async function embedText(_q: string): Promise<number[] | null> {
+  return null // TODO: idem
+}
